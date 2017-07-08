2c8ff7693f41904f959e6a9e6c6056239f0f08aa
Alisan Dagdelen <alisandagdelen@gmail.com>
2017-07-08 17:56:21 +0300

    git init

---
+++ BaseProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BaseProject
+//
+//  Created by alişan dağdelen on 8.07.2017.
+//  Copyright © 2017 alisandagdeleb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
