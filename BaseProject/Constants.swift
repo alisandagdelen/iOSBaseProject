121163d9012c8c6dbe0d40567d5fda7bf54b1758
Alisan Dagdelen <alisandagdelen@gmail.com>
2017-07-09 14:04:32 +0300

    api url added as a constant

---
+++ BaseProject/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  BaseProject
+//
+//  Created by alişan dağdelen on 9.07.2017.
+//  Copyright © 2017 alisandagdeleb. All rights reserved.
+//
+
+import Foundation
+
+enum API {
+    static let BaseURL = "https://example.com/v1/"    
+}
