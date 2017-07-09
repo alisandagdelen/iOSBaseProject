f4257a1fb105aec4da096d464fd8fa9a31496581
Alisan Dagdelen <alisandagdelen@gmail.com>
2017-07-09 14:04:10 +0300

    realm helper

---
+++ BaseProject/RealmHelper.swift
@@ -0,0 +1,13 @@
+//
+//  RealmHelper.swift
+//  BaseProject
+//
+//  Created by alişan dağdelen on 9.07.2017.
+//  Copyright © 2017 alisandagdeleb. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+import Realm
+
+var _realm:Realm!
